ec3dd389eb0dff139c65f9f0c4ad95dc222808c1
manuel michel <mail@manuelmichel.de>
2013-01-31 02:23:26 +0100

    working on NewsFeed

---
+++ TagExplorerProcessing2/src/tagexplorerprocessing2/News.java
@@ -0,0 +1,51 @@
+package tagexplorerprocessing2;
+
+import java.sql.Timestamp;
+
+public class News {
+	
+	Timestamp ts;
+	String type; // symbol
+	Tag_User user;
+	String comment;
+	
+	
+	public News(Timestamp ts){
+		this.ts = ts;
+		this.type = generateType();
+		this.user = generateUser();
+		this.comment = generateComment();
+	}
+	
+	
+	public News(Timestamp ts, String type){
+		this.ts = ts;
+		this.type = type;
+		this.user = generateUser();
+		this.comment = generateComment();
+	}
+	
+	public News(Timestamp ts, String type, Tag_User user, String comment){
+		this.ts = ts;
+		this.type = type;
+		this.user = user;
+		this.comment = comment;
+	}
+	
+	
+	public Tag_User generateUser(){	
+		Tag_User user = new Tag_User("users", 0, "name", "pw");
+		return user;
+	}
+	
+	public String generateType(){	
+		String type = "call"; // "call", "message", "file"
+		return type;
+	}
+	
+	public String generateComment(){	
+		String comment = "comment";
+		return comment;
+	}
+
+}
--- TagExplorerProcessing2/src/tagexplorerprocessing2/NewsFeed.java
+++ TagExplorerProcessing2/src/tagexplorerprocessing2/NewsFeed.java
@@ -1,5 +1,8 @@
 package tagexplorerprocessing2;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 import processing.core.PConstants;
 import toxi.geom.Vec2D;
 
@@ -9,10 +12,27 @@ public class NewsFeed extends Vec2D{
 	int w = 380;
 	
 	Button_Image newButton;
+	ArrayList<News> news = new ArrayList<News>();
 	
 	public NewsFeed(TagExplorerProcessing2 p5, int x, int y){
 		super(x, y);
 		this.p5 = p5;
+		
+		
+		
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 6L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 8L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 16L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 24L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 26L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 27L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 44L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 49L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 50L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 66L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 70L * 60 * 60 * 1000)));
+		news.add(new News(new Timestamp(System.currentTimeMillis() - 73L * 60 * 60 * 1000)));
+		
 		
 		newButton = new Button_Image(p5, "news", x-80 , y);
 	}
@@ -41,11 +61,18 @@ public class NewsFeed extends Vec2D{
 		p5.textAlign(PConstants.CENTER);
 		p5.textFont(p5.font, 12);
 		p5.fill(p5.cFont);
-		for(int i = 0; i<12; i++){
-			p5.text("12. 1. 2013", x-40, y + 50 + yShift);
+		
+		p5.sdf.applyPattern("dd. MM. yyyy");
+		
+		for(News n : news){
+			p5.text(p5.sdf.format(n.ts), x-40, y + 50 + yShift);
 			yShift += 60;
 		}
 		
+//		for(int i = 0; i<12; i++){
+//			p5.text("12. 1. 2013", x-40, y + 50 + yShift);
+//			yShift += 60;
+//		}	
 		newButton.render();
 		
 	}
